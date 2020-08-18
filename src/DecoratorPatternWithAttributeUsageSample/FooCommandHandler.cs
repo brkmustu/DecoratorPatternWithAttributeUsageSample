e6788fe905943a03fcc491bdd55e87577d450a19
Mehmet Burak Muştu <47041552+brkmustu@users.noreply.github.com>
2020-08-18 03:52:23 +0300

    Update FooCommandHandler.cs

--- src/DecoratorPatternWithAttributeUsageSample/FooCommandHandler.cs
+++ src/DecoratorPatternWithAttributeUsageSample/FooCommandHandler.cs
@@ -8,6 +8,7 @@ namespace DecoratorPatternWithAttributeUsageSample
         {
             System.Console.WriteLine("Foo name is : " + command.Name);
             System.Console.WriteLine("Foo code is : " + command.Code);
+            throw new System.Exception(); /// kod bloğunun exception decorator'e uğradığını anlamak maksadıyla ekledik.
         }
     }
 }
